85881da300cc5bd735c9b176e10265b5349fc66b
Time Lord <2393686479@qq.com>
2024-06-02 13:00:32 +0800

    feat: icon组件开发

--- packages/components/Button/Button.vue
+++ packages/components/Button/Button.vue
@@ -3,6 +3,7 @@
     :is="tag"
     ref="_ref"
     class="er-button"
+    :autofocus="autofocus"
     :type="tag === 'button' ? nativeType : void 0"
     :disabled="disabled || loading ? true : false"
     :class="{
@@ -14,20 +15,44 @@
       'is-disabled': disabled,
       'is-loading': loading,
     }"
+    @click="
+      (e: MouseEvent) =>
+        useThrottle ? handleBtnClickThrottle(e) : handleBtnClick(e)
+    "
   >
+    <template v-if="loading">
+      <slot name="loading">
+        <er-icon
+          class="loading-icon"
+          :icon="loadingIcon ?? 'spinner'"
+          :style="iconStyle"
+          spin
+        />
+      </slot>
+    </template>
+    <er-icon
+      v-if="icon && !loading"
+      :icon="icon"
+      :style="iconStyle"
+      size="1x"
+    />
     <slot></slot>
   </component>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import type { ButtonProps, ButtonEmits, ButtonInstance } from "./types";
 import { throttle } from "lodash-es";
+import ErIcon from "../Icon/Icon.vue";
+
 defineOptions({ name: "ErButton" });
 
 const props = withDefaults(defineProps<ButtonProps>(), {
   tag: "button",
   nativeType: "button",
+  useThrottle: true,
+  throttleDuration: 500,
 });
 
 const slots = defineSlots();
@@ -36,6 +61,13 @@ const _ref = ref<HTMLButtonElement>();
 
 const emits = defineEmits<ButtonEmits>();
 
+const handleBtnClick = (e: MouseEvent) => emits("click", e);
+
+const handleBtnClickThrottle = throttle(handleBtnClick, props.throttleDuration);
+
+const iconStyle = computed(() => ({
+  marginRight: slots.default ? "6px" : "0px",
+}));
 defineExpose<ButtonInstance>({
   ref: _ref,
 });
